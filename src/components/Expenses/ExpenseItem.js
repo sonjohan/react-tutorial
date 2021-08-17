6e9eb882727800b8469ae0923f95eb1bde70c041
sonjohan <sonjohan@hotmail.com>
2021-08-17 18:31:17 -0400

    section 4, lifting the state up

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../Ui/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('updated! ' + Math.floor(Math.random() * (10 - 0 + 1) + 0));
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
